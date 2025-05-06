7df7db7a1dc61adefa086b65da9d2cab3c28cf26
MelonMimic <sol5415@gmail.com>
2025-05-06 23:20:50 +1000

    Implemented movement relative to camera
    To Do:
    Create book task
    Game recognises when books are organised

--- Assets/Scripts/ObjectInteractions.cs
+++ Assets/Scripts/ObjectInteractions.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal.Internal;
 
 public class ObjectInteractions : MonoBehaviour
 {
@@ -23,7 +24,7 @@ public class ObjectInteractions : MonoBehaviour
         if (playerStatus_PI == null)
         {
             playerStatus_PI = FindObjectOfType<PlayerInteraction>();
-            //To be used when implementing movement into the interactable script - I
+            //To be used when implementing movement into the interactable script
         }
 
     }
@@ -32,21 +33,37 @@ public class ObjectInteractions : MonoBehaviour
     void Update()
     {
         //Move();
-        //not really sure if I needed to make this its own function but I did it anyway - I 
     }
 
 
-    //Can we make it based on the camera? That way we can add basic camera controls if we want later?
+    //Can we make it based on the camera? That way we can add basic camera controls if we want later? 
+    //Done
     public void Move()
     {
+        float verticalInput = Input.GetAxis("Vertical");
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float upInput = Input.GetAxis("Jump");
+
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
+        Vector3 up = Camera.main.transform.up;
+        forward.y = 0;
+        right.y = 0;
+        forward = forward.normalized;
+        right = right.normalized;
+        up = up.normalized;
+
+        Vector3 forwardRelativeInput = verticalInput * forward;
+        Vector3 rightRelativeInput = horizontalInput * right;
+        Vector3 upRelativeInput = upInput * up;
+
         rb.constraints = RigidbodyConstraints.FreezeRotation;
         //when rotation is not frozen the cube spins all over the place - I
-        playerInput = new Vector3(Input.GetAxis("Horizontal") * -1, Input.GetAxis("Jump"), Input.GetAxis("Vertical") * -1);
+        Vector3 playerInput = forwardRelativeInput + rightRelativeInput + upRelativeInput;
         characterController.Move(playerInput * Time.deltaTime * moveSpeed);
-        //The horizontal and vertical axes are multiplied by -1, otherwise the movement is inverted and it feels weird - I
     }
 
 
-    //The character controller seems to mess with an object with the interactable script while floating so if we want to use this movement method we'll have to do something about that - I
+
 
 }
