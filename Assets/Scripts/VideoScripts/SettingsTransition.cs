2f4e72657e853c845cbac811408f3be1d301260d
MelonMimic <sol5415@gmail.com>
2025-08-19 20:35:11 +1000

    started work on main menu (UNFINISHED)
    Problems:
    -On my end when I was working on this Unity became very slow and my memory usage spiked up, I'm not sure if that's an issue with my computer or if I have done something for the game to be horribly optimised which if that's the case I am sorry
    -The transition between videos is not smooth because you can actually the first video disappear and then the next one appear, it is obvious they're loading in.
    -I have not yet made it so that the back button plays another transition

---
+++ Assets/Scripts/VideoScripts/SettingsTransition.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlaySettingsScreen : MonoBehaviour
+{
+    VideoPlayer videoPlayer;
+    public GameObject hud;
+    public VideoClip transition;
+    public VideoClip loop;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.clip = transition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        videoPlayer.loopPointReached += OnLoopPointReached;
+    }
+
+    void OnLoopPointReached(VideoPlayer source)
+    {
+        Debug.Log("video ended");
+        if (videoPlayer.clip == transition)
+        {
+            videoPlayer.clip = loop;
+        }
+    }
+}
